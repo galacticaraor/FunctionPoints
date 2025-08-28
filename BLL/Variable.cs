c5b70ddec2a016bf1dba2e7b1c7ec94576178db4
galacticaraor <apfan@DESKTOP-NGNRCN7>
2025-08-28 17:33:41 -0500

    Added 2 new classes

---
+++ BLL/Variable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    internal class Variable
+    {
+        public int Coefficient { get; set; }
+        public int Exponent { get; set; }
+    }
+}
